9417e0caa30423eab6fa518b222d2578c14b60b2
torikul0191012 <torikulislam.official19@gmail.com>
2025-08-29 11:23:03 +0600

    Call History Section

--- script.js
+++ script.js
@@ -25,10 +25,29 @@ for ( const btn of call ){
         alert( serviceName + " ।। " + "হটলাইন নাম্বার" +" - "+ serviceNumber + " ।। " + "আপনার প্রতিটি কলের জন্য ২০ কয়েন কেটে নেয়া হবে ")
         coinCount = coinCount - 20
         document.getElementById('coin-count').innerText = coinCount
+
+// Call History Section //
+    const callHistory = document.getElementById('call-history');  
+    const clearBtn = document.getElementById('clear-history');
+//For Time
+    const timeNow = new Date().toLocaleTimeString();
+//Add History
+    const historyItem = document.createElement("div");
+            historyItem.className = "p-2 border-b flex justify-between items-center";  //for style
+            historyItem.innerHTML = `
+                <div>
+                    <p class="font-semibold">${serviceName}</p>
+                    <p class="text-sm text-gray-500">${serviceNumber}</p>
+                </div>
+                <span class="text-xs text-gray-400">${timeNow}</span>
+            `;
+// Call history তে append /prepend 
+            callHistory.prepend(historyItem);
     }    
     else{
          alert("আপনার কাছে পর্যাপ্ত কয়েন নেই!")
     }    
+
 })}
 
 
